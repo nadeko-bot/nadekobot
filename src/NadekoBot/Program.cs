1e982174f66e84cab92be7720d3fae762010545e
Hunter T. <strangeranger@hthompson.dev>
2025-02-23 14:06:39 -0800

    refactor: remove Program class in favor of top level statements

--- src/NadekoBot/Program.cs
+++ src/NadekoBot/Program.cs
@@ -1,47 +1,39 @@
-namespace NadekoBot;
-
-class Program
+static string GetAppVersion()
 {
-    private static string GetAppVersion()
+    return typeof(Program)
+        .Assembly
+        .GetName()
+        .Version?
+        .ToString() ?? "Unknown version";
+}
+
+if (args.Length > 0 && args[0] == "--version")
+{
+    Console.WriteLine(GetAppVersion());
+    return;
+}
+
+var shardId = 0;
+int? totalShards = null; // 0 to read from creds.yml
+
+if (args.Length > 0 && args[0] != "run")
+{
+    if (!int.TryParse(args[0], out shardId))
     {
-        return typeof(Program)
-            .Assembly
-            .GetName()
-            .Version?
-            .ToString() ?? "Unknown";
+        Console.Error.WriteLine("Invalid first argument (shard id): {0}", args[0]);
+        return;
     }
-    
-    public static async Task Main(string[] args)
+
+    if (args.Length > 1)
     {
-        if (args.Length > 0 && args[0] == "--version")
+        if (!int.TryParse(args[1], out var shardCount))
         {
-            Console.WriteLine(GetAppVersion());
+            Console.Error.WriteLine("Invalid second argument (total shards): {0}", args[1]);
             return;
         }
 
-        var shardId = 0;
-        int? totalShards = null; // 0 to read from creds.yml
-
-        if (args.Length > 0 && args[0] != "run")
-        {
-            if (!int.TryParse(args[0], out shardId))
-            {
-                Console.Error.WriteLine("Invalid first argument (shard id): {0}", args[0]);
-                return;
-            }
-
-            if (args.Length > 1)
-            {
-                if (!int.TryParse(args[1], out var shardCount))
-                {
-                    Console.Error.WriteLine("Invalid second argument (total shards): {0}", args[1]);
-                    return;
-                }
-
-                totalShards = shardCount;
-            }
-        }
-
-        await new Bot(shardId, totalShards).RunAndBlockAsync();
+        totalShards = shardCount;
     }
 }
+
+await new Bot(shardId, totalShards).RunAndBlockAsync();
